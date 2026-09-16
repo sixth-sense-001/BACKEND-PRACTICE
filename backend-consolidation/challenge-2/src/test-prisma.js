8cf6c9d2cbed02046a5c166f9beddd831138804d
Prince Bright <p97741053@gmail.com>
2026-09-16 07:48:48 +0000

    Create the prisma client and test it.

---
+++ backend-consolidation/challenge-2/src/test-prisma.js
@@ -0,0 +1,10 @@
+import prisma from './lib/prismaClient.js';
+
+try {
+    await prisma.$connect();
+    console.log('Prisma is running successfully');
+} catch(error) {
+    console.log('Prisma failed to connect!');
+} finally {
+    await prisma.$disconnect();
+}
